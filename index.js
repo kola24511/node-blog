c09950b7a646ebc5d93493730be1bedc05472a76
kola2451 <kolyanemov7@gmail.com>
2023-07-29 17:46:40 +0300

    Создание подключения к MongoDB

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('start')
+const express = require('express')
+const mongoose = require('mongoose')
+const env = require("dotenv")
+const app = express()
+env.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+}).then(console.log("БД подключена")).catch((err) => console.log(err))
+
+app.listen("3000", () => {
+    console.log('Cервер запущен')
+})
