e59a40656c39ea20944e72db006faf274fed3d0a
kola2451 <kolyanemov7@gmail.com>
2023-07-29 18:05:48 +0300

    Добавление модели поста

---
+++ api/models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    description: {
+        type:String,
+        required:true
+    },
+    image: {
+        type:String,
+        required:false
+    },
+    author: {
+        type:String,
+        required:true
+    },
+}, {timestamps: true } );
+
+module.exports = mongoose.model("Post", PostSchema);
