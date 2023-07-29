dcb9a06b0ee29baf5ece8a75bb73e0672f4e8a05
kola2451 <kolyanemov7@gmail.com>
2023-07-29 18:18:32 +0300

    Добавление api для аутентификации

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const env = require("dotenv")
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const env = require("dotenv");
+const app = express();
+const authRoute = require("./api/routes/auth");
 env.config();
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
 }).then(console.log("БД подключена")).catch((err) => console.log(err))
+
+app.use("api/auth", authRoute);
 
 app.listen("3000", () => {
     console.log('Cервер запущен')
