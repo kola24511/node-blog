e485a76dea6f989b31fa162a9c589aa41ad6e7e7
kola2451 <kolyanemov7@gmail.com>
2023-08-17 19:26:44 +0300

    User Updated

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const env = require("dotenv");
 const app = express();
 const authRoute = require("./api/routes/auth");
+const userRoute = require("./api/routes/users");
 
 env.config();
-
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
@@ -13,6 +13,7 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(console.log("БД подключена")).catch((err) => console.log(err))
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen("3000", () => {
     console.log('Сервер запущен')
