d311f9d37a4475972db4569d89356838d13e2dae
kola2451 <kolyanemov7@gmail.com>
2023-08-17 19:34:24 +0300

    add delete user

--- api/routes/users.js
+++ api/routes/users.js
@@ -9,9 +9,13 @@ router.put("/:id", async (req, res) => {
             req.body.password = await bcrypt.hash(req.body.password, salt);
         }
         try {
-            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-                $set: req.body,
-            });
+            const updatedUser = await User.findByIdAndUpdate(
+                req.params.id, 
+                {
+                   $set: req.body,
+                }, 
+                {new:true}
+            );
             res.status(200).json(updatedUser);
         } catch(err) {
             res.status(500).json(err);
@@ -20,5 +24,25 @@ router.put("/:id", async (req, res) => {
         res.status(401).json("Вы можете обновить только свой аккаунт!")
     }
 });
+
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+      try {
+        const user = await User.findById(req.params.id);
+        try {
+          await Post.deleteMany({ username: user.username });
+          await User.findByIdAndDelete(req.params.id);
+          res.status(200).json("Пользователь успешно удален");
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } catch (err) {
+        res.status(404).json("Пользователь не найден!");
+      }
+    } else {
+      res.status(401).json("Вы удаляете не свой аккаунт!");
+    }
+  });
+  
 
 module.exports = router;
