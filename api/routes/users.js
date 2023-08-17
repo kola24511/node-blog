373ecca469de69919c4bc65c564527325a2c41bd
kola2451 <kolyanemov7@gmail.com>
2023-08-17 20:04:43 +0300

    add get user

--- api/routes/users.js
+++ api/routes/users.js
@@ -42,7 +42,18 @@ router.delete("/:id", async (req, res) => {
     } else {
       res.status(401).json("Вы удаляете не свой аккаунт!");
     }
-  });
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
   
 
 module.exports = router;
